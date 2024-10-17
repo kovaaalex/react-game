ea06188eac2d87a0019468e0176d737c6105ee8d
kovaaalex <kovalenko.alex04@gmail.com>
2024-10-17 03:51:23 +0300

    add end of tic-tac

--- src/components/TicTac/Board.tsx
+++ src/components/TicTac/Board.tsx
@@ -4,12 +4,55 @@ const Board: React.FC<{size: number}> = (props) => {
     const [cells, updateSells] = useState(Array(props.size **2).fill(null))
     const [move, changeMove] = useState(true)
     const [isEnd, checkIsEnd] = useState(false)
+    const checkEnd = (newCells: string[], index: number) => {
+        const rowIndex = Math.floor(index / props.size)
+        const columnIndex = index % props.size
+        //Check row
+        const arrRow = newCells.slice(rowIndex * props.size, rowIndex * props.size + props.size)
+        if (arrRow.every(element => element === newCells[index]) && newCells[index] !== null) {
+            checkIsEnd(true)
+            alert(`${newCells[index]} is win`)
+            return
+        }
+        //Check Column
+        let arrColumn = []
+        for (let j = 0; j < props.size; j++) {
+            arrColumn.push(newCells[j * props.size + columnIndex])
+        }
+        if(arrColumn.every(element => element === newCells[index]) && newCells[index] !== null) {
+            if(!isEnd)
+            checkIsEnd(true)
+            alert(`${cells[index]} is win`)
+            return
+        }
+        let mainDiagonal = []
+        for (let j = 0; j < props.size; j++) {
+            mainDiagonal.push(newCells[j * (props.size + 1)])
+        }
+        if(mainDiagonal.every(element => element === newCells[index]) && newCells[index] !== null) {
+            checkIsEnd(true)
+            alert(`${cells[index]} is win`)
+            return
+        }
+        let antiDiagonal = []
+        for (let j = 0; j < props.size; j++) {
+            antiDiagonal.push(newCells[j * props.size - (props.size - 1 - j)])
+        }
+        if(antiDiagonal.every(element => element === newCells[index]) && newCells[index] !== null) {
+            checkIsEnd(true)
+            alert(`${cells[index]} is win`)
+            return
+        }
+        //Check diagonales
+        
+    }
     const handleMove = (index: number) => {
         if(isEnd && cells[index]) return
         const newCells = [...cells]
         newCells[index] = move ? 'x' : 'o'
         updateSells(newCells)
         changeMove(!move)
+        checkEnd(newCells, index)
     }
     const renderCells = () => {
         return cells.map((cell, index) => {
